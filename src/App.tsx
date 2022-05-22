ce09974ebb09e15f3488d13a359e0d146e682d88
Hadayet Ali <hadayethm1999@gmail.com>
2022-05-22 16:54:22 +0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,49 @@ import './App.css';
 
 const App:FC=()=> {
   return (
-    <div className="App">Hello
+    <div className="App">
+      <div className='top_header'> 
+      <div className='container-fluid'> 
+        <div className='row'>
+            <div className='col-lg-12'>
+              <div className='top_text text-center'>
+                <h3>Calculator</h3>
+              </div>
+            </div>
+        </div>
+      </div>
+      </div>
+      <div className='main_area'>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-lg-6 offset-lg-3'>
+                  <div className='calculator_form_area'>
+                    <p>Enter the numbers</p>
+                    <form id='main_form'>
+                      <div className='form-group'>
+                        <input type="text" className='form-control' placeholder='number 1' />
+                      </div>
+                      <div className='form-group'>
+                        <input type="text" className='form-control' placeholder='number 2' />
+                      </div>
+                      <div className='submit-button'>
+                        <button type='submit' className='submit_btn'>Sum</button>
+                      </div>
+                      <div className='border_bottom'></div>
+                      <div className='search_result'>
+                        <p>Results</p>
+                        <div className='form-group'>
+                        <input type="text" className='form-control' placeholder='1523' />
+                      </div>
+                      </div>
+                      <div className='border_bottom'></div>
+                      <p className='mt-4'>History</p>
+                    </form>
+                  </div>
+                </div>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
